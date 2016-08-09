1916de51b4950a56fa22eb6e2da4ebcb422be3a6
berbb <tintinridge@gmail.com>
2016-08-09 18:15:42 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace PortAIO_Loader
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(System.EventArgs args)
+        {
+            PortAIO.Init.Initialize();
+        }
+    }
+}
